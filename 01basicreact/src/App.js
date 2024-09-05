478f7cc4939246abe58a362ddc777bbe978c7db4
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2024-09-05 15:36:09 +0530

    learn how to add react and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello React | Ayush Yadav</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello React with vite | Ayush Yadav</h1>
+  )
+}
+
+export default App
