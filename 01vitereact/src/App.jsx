ad06ef25adb5d5d9b13c58dea7a5a8c334d87415
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2024-09-06 20:52:53 +0530

    create cutosm react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,14 @@
 import Chai from "./chai"
+
 function App() {
+  const username = "chai aur code";
+
   return (
+    <>
     <Chai />
+    <h1>chai aur react - {username}</h1>
+    <p>Ayush Yadav</p>
+    </>
   )
 }
 
