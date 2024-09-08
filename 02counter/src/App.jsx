5bfa272cf9baefd9280813b7c0ce0e6911699c40
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2024-09-08 14:48:47 +0530

    add some feature

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  // let counter = 5
+
+  const addValue = () => {
+    setCounter(prevCounter => prevCounter + 1);
+  };
+
+  const removeValue = () => {
+    setCounter(prevCounter => (prevCounter > 0 ? prevCounter - 1 : 0));
+  };
+
+  return (
+    <>
+      <h1>Chai aur code</h1>
+      <h2>Counter : {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add Value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Decrease value</button>
+
+    </>
+  )
+}
+
+export default App
