ef317777563aaec6e52c5894f445e452b7596fa4
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2024-09-22 12:23:42 +0530

    add

--- 10todocontext/src/App.jsx
+++ 10todocontext/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState([])
 
+
+
   const addTodo = (todo) => {
     setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
   }
