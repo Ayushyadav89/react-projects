2c0ba43d8f2e34795b9bebfcb528ecb30bf2bedb
Ayushyadav89 <2k22.cse.2213307@gmail.com>
2024-09-27 18:36:38 +0530

    setup redux toolKIt

---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,10 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{id: 1, text: "Hello World"}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState
+})
